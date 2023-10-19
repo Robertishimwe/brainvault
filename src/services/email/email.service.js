614693bbffd358bf6a5580786d671bbec0484b71
Robertisimwe <robertishimwe0@gmail.com>
2023-10-19 13:29:13 +0200

    osto

--- src/services/email/email.service.js
+++ src/services/email/email.service.js
@@ -1,9 +1,11 @@
 import nodemailer from 'nodemailer'; 
 import Transport from "nodemailer-sendinblue-transport";
 
+require('dotenv').config()
+
 const emailService = (sendTo, subject, htmlTemp) => {
 	const transporter = nodemailer.createTransport(
-		new Transport({ apiKey: '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' })
+		new Transport({ apiKey: process.env.SEND_IN_BLUE })
 	);
 
 	const footer = `<p>This email was sent from Brain vault.</p>`;
